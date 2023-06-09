569d0d45458b1ff3c280126c097017f2c98be83c
Tiejun Gao <tiejun.gao@dolby.com>
2023-06-09 15:50:59 +1000

    Add time to executable for more info

--- build.sh
+++ build.sh
@@ -3,10 +3,11 @@
 # go get github.com/sirupsen/logrus
 
 mkdir -p build
+build_time=$(date '+%F_%H:%M:%S')
+build_hash=$(git rev-parse HEAD)
+GOOS=linux GOARCH=amd64 go build -ldflags="-X main.Commit=${build_time}__$build_hash" -o build/sc.linux
 
-GOOS=linux GOARCH=amd64 go build -ldflags="-X main.Commit=$(git rev-parse HEAD)" -o build/sc.linux
+GOOS=windows GOARCH=amd64 go build -ldflags="-X main.Commit=${build_time}__$build_hash" -o build/sc.exe
 
-GOOS=windows GOARCH=amd64 go build -ldflags="-X main.Commit=$(git rev-parse HEAD)" -o build/sc.exe
-
-GOOS=darwin GOARCH=amd64 go build -ldflags="-X main.Commit=$(git rev-parse HEAD)" -o build/sc.mac
+GOOS=darwin GOARCH=amd64 go build -ldflags="-X main.Commit=${build_time}__$build_hash" -o build/sc.mac
 
